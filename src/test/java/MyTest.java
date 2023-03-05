c4fd13f8b1ad862aaef2e3759dd3fca9684cc000
Seyfal Sultanov <ssulta24@uic.edu>
2023-03-05 01:56:32 -0600

    pom.xml updated the artifact id as per the project file and added KenoGame.java class

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -7,11 +7,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class MyTest {
+public class MyTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	// write a test to check if the project can be built
+	public static boolean isJUnitTest() {
+		for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
+			if (element.getClassName().startsWith("org.junit.")) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
