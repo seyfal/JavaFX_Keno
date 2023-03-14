52fa4289c53d4123f73b750c92049db1d9db5215
Seyfal Sultanov <ssulta24@uic.edu>
2023-03-14 01:43:49 -0500

    Reworked the JavaFXTemplate

--- src/main/java/JavaFXTemplate.java
+++ src/main/java/JavaFXTemplate.java
@@ -3,175 +3,178 @@ import javafx.animation.PauseTransition;
 import javafx.animation.RotateTransition;
 import javafx.animation.SequentialTransition;
 import javafx.application.Application;
-
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-
-import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
-
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.StrokeType;
 import javafx.scene.text.FontWeight;
 
+/*
+UNUSED IMPORTS
 import java.util.Random;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.image.Image;
+*/
+
+public
+class JavaFXTemplate extends Application {
+    private BorderPane root; // root node of scene graph
+    private Text       welcomeText; // welcome text
+    private Button     backButton; // back button
+    private int        newLookNum; // number of times new-look button has been clicked
+
+    public static
+    void main (String[] args) { // entry point of JavaFX application
+        launch(args); // launch JavaFX application
+    }
 
 
-public class JavaFXTemplate extends Application {
-	private BorderPane root;
-	private Text welcomeText;
-	private Button backButton;
-	private int newLookNum;
+    @Override
+    public
+    // TODO: Take a look at the exception as it is never thrown in the mentioned method
+    void start (Stage primaryStage) throws Exception { // called by launch()
+        primaryStage.setTitle("Welcome to JavaFX"); // set title of window
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+        // @
+        Rectangle rect = new Rectangle(100, 40, 100, 100); // create rectangle
+        rect.setArcHeight(50); // set arc height
+        rect.setArcWidth(50); // set arc width
+        rect.setFill(Color.VIOLET); // set fill color
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Welcome to JavaFX");
+        RotateTransition rt = new RotateTransition(Duration.millis(5000), rect); // create rotate transition
+        rt.setByAngle(270); // set by angle
+        rt.setCycleCount(4); // set cycle count
+        rt.setAutoReverse(true); // set auto reverse
+        SequentialTransition seqTransition = new SequentialTransition(new PauseTransition(Duration.millis(500)), rt); // create sequential transition
+        seqTransition.play(); // play sequential transition
 
-		Rectangle rect = new Rectangle(100, 40, 100, 100);
-		rect.setArcHeight(50);
-		rect.setArcWidth(50);
-		rect.setFill(Color.VIOLET);
+        FadeTransition ft = new FadeTransition(Duration.millis(5000), rect); // create fade transition
+        ft.setFromValue(1.0); // set from value
+        ft.setToValue(0.3); // set to value
+        ft.setCycleCount(4); // set cycle count
+        ft.setAutoReverse(true); // set auto reverse
 
-		RotateTransition rt = new RotateTransition(Duration.millis(5000), rect);
-		rt.setByAngle(270);
-		rt.setCycleCount(4);
-		rt.setAutoReverse(true);
-		SequentialTransition seqTransition = new SequentialTransition(
-				new PauseTransition(Duration.millis(500)),
-				rt
-		);
-		seqTransition.play();
+        ft.play(); // play fade transition
+        root = new BorderPane(); // create root node of scene graph
 
-		FadeTransition ft = new FadeTransition(Duration.millis(5000), rect);
-		ft.setFromValue(1.0);
-		ft.setToValue(0.3);
-		ft.setCycleCount(4);
-		ft.setAutoReverse(true);
+        // add menu bar
+        MenuBar menuBar = new MenuBar(); // create menu bar
+        Menu fileMenu = new Menu("Menu"); // create file menu
+        MenuItem rules = new MenuItem("Game Rules"); // create rules menu item
+        MenuItem odds = new MenuItem("Odds of Winning"); // create odds menu item
+        MenuItem newLook = new MenuItem("New Look"); // create new-look menu item
+        MenuItem exit = new MenuItem("Exit"); // create exit menu item
+        fileMenu.getItems().addAll(rules, odds, newLook, exit); // add menu items to file menu
+        menuBar.getMenus().addAll(fileMenu); // add file menu to menu bar
+        root.setTop(menuBar); // add menu bar to root node of scene graph
 
-		ft.play();
+        // add welcome text
+        welcomeText = new Text("Welcome to Keno"); // create welcome text
+        welcomeText.setFont(Font.font("Verdana", FontWeight.BOLD, 40)); // set font of welcome text
+        welcomeText.setFill(Color.WHITE); // set fill color of welcome text
+        welcomeText.setStroke(Color.BLACK); // set stroke color of welcome text
+        welcomeText.setStrokeType(StrokeType.OUTSIDE); // set stroke type of welcome text
+        welcomeText.setStrokeWidth(3); // set stroke width of welcome text
+        root.setCenter(welcomeText); // add welcome text to root node of scene graph
 
-		root = new BorderPane();
+        // add play button
+        Button playButton = new Button("Play"); // create play button
+        playButton.setFont(new Font(30)); // set font of play button
+        playButton.setOnAction(event -> { // add event handler to play button
+            root.setCenter(null); // remove welcome text from root node of scene graph
+            //TODO: add code to start game
+        });
 
-		// add menu bar
-		MenuBar menuBar = new MenuBar();
-		Menu fileMenu = new Menu("Menu");
-		MenuItem rules = new MenuItem("Game Rules");
-		MenuItem odds = new MenuItem("Odds of Winning");
-		MenuItem newLook = new MenuItem("New Look");
-		MenuItem exit = new MenuItem("Exit");
-		fileMenu.getItems().addAll(rules, odds, newLook, exit);
+        VBox vBox = new VBox(welcomeText, playButton); // create vertical box
+        vBox.setAlignment(Pos.CENTER); // set alignment of vertical box
+        root.setCenter(vBox); // add vertical box to root node of scene graph
+        newLookNum = 0; // initialize new-look number
+        String backgroundPath = "/KenoBackground" + newLookNum + ".jpg"; // create path to background image
+        root.setStyle("-fx-background-image: url('" + backgroundPath + "');" + // set background image
+                "-fx-background-size: cover;" + // set background size
+                "-fx-background-position: left center;"); // set background position
+        Scene scene = new Scene(root, 700, 700); // create scene
+        primaryStage.setScene(scene); // add scene to stage
+        primaryStage.show(); // display stage
 
-		menuBar.getMenus().addAll(fileMenu);
-		root.setTop(menuBar);
+        // add event handlers for menu items
+        rules.setOnAction(event -> { // add event handler to rules menu item
 
-		// add welcome text
-		welcomeText = new Text("Welcome to Keno");
-		welcomeText.setFont(Font.font("Verdana", FontWeight.BOLD, 40));
-		welcomeText.setFill(Color.WHITE);
-		welcomeText.setStroke(Color.BLACK);
-		welcomeText.setStrokeType(StrokeType.OUTSIDE);
-		welcomeText.setStrokeWidth(3);
-		root.setCenter(welcomeText);
+            root.getChildren().remove(menuBar); // remove menu bar from root node of scene graph
+            welcomeText.setText("Rules"); // set text of welcome text
+            root.setCenter(welcomeText); // add welcome text to root node of scene graph
+            backButton = new Button("Back"); // create back button
 
-		// add play button
-		Button playButton = new Button("Play");
-		playButton.setFont(new Font(30));
-		playButton.setOnAction(event -> {
-			welcomeText.setText("Play Screen");
-			// remove "Play" button without removing the welcome text
-			root.setCenter(null);
+            backButton.setOnAction(event1 -> { // add event handler to back button
+                root.setTop(menuBar); // add menu bar to root node of scene graph
+                welcomeText.setText("Welcome to Keno"); // set text of welcome text
+                root.setLeft(null); // remove back button from root node of scene graph
+                vBox.getChildren().clear(); // remove welcome text and play button from vertical box
+                vBox.getChildren().addAll(welcomeText, playButton); // add welcome text and play button to vertical box
+                root.setCenter(vBox); // add vertical box to root node of scene graph
+            });
 
+            VBox vbox = new VBox(10, backButton); // create vertical box
+            root.setLeft(vbox); // add vertical box to root node of scene graph
+        });
 
-		});
+        odds.setOnAction(event -> { // add event handler to odds menu item
 
+            root.getChildren().remove(menuBar); // remove menu bar from root node of scene graph
+            welcomeText.setText("Odds"); // set text of welcome text
+            root.setCenter(welcomeText); // add welcome text to root node of scene graph
+            backButton = new Button("Back"); // create back button
 
-		VBox vBox = new VBox(welcomeText, playButton);
-		vBox.setAlignment(Pos.CENTER);
-		root.setCenter(vBox);
-		newLookNum = 0;
-		String backgroundPath = "/KenoBackground" + newLookNum + ".jpg";
-		root.setStyle("-fx-background-image: url('" + backgroundPath + "');" +
-				"-fx-background-size: cover;" +
-				"-fx-background-position: left center;");
+            backButton.setOnAction(event1 -> { // add event handler to back button
 
-		Scene scene = new Scene(root, 700, 700);
+                root.setTop(menuBar); // add menu bar to root node of scene graph
+                welcomeText.setText("Welcome to Keno"); // set text of welcome text
+                root.setLeft(null); // remove back button from root node of scene graph
+                vBox.getChildren().clear(); // remove welcome text and play button from vertical box
+                vBox.getChildren().addAll(welcomeText, playButton); // add welcome text and play button to vertical box
+                root.setCenter(vBox); // add vertical box to root node of scene graph
 
-		primaryStage.setScene(scene);
+            });
 
-		primaryStage.show();
+            VBox vbox = new VBox(10, backButton); // create vertical box
+            root.setLeft(vbox); // add vertical box to root node of scene graph
+        });
 
-		// add event handlers for menu items
-		rules.setOnAction(event -> {
-			root.getChildren().remove(menuBar);
-			welcomeText.setText("Rules");
-			root.setCenter(welcomeText);
-			backButton = new Button("Back");
-			backButton.setOnAction(event1 -> {
-				root.setTop(menuBar);
-				welcomeText.setText("Welcome to Keno");
-				root.setLeft(null);
-				vBox.getChildren().clear();
-				vBox.getChildren().addAll(welcomeText, playButton);
-				root.setCenter(vBox);
-			});
-			VBox vbox = new VBox(10, backButton);
-			root.setLeft(vbox);
-		});
+        newLook.setOnAction(event -> { // add event handler to new-look menu item
 
-		odds.setOnAction(event -> {
+            newLookNum = (newLookNum + 1) % 4; // increment new-look number
+            String newBackgroundPath = "/KenoBackground" + newLookNum + ".jpg"; // create path to background image
 
-			root.getChildren().remove(menuBar);
-			welcomeText.setText("Odds");
-			root.setCenter(welcomeText);
-			backButton = new Button("Back");
-			backButton.setOnAction(event1 -> {
-				root.setTop(menuBar);
-				welcomeText.setText("Welcome to Keno");
-				root.setLeft(null);
-				vBox.getChildren().clear();
-				vBox.getChildren().addAll(welcomeText, playButton);
-				root.setCenter(vBox);
-			});
-			VBox vbox = new VBox(10, backButton);
-			root.setLeft(vbox);
-		});
+            root.setStyle("-fx-background-image: url('" + newBackgroundPath + "');" + // set background image
+                    "-fx-background-size: cover;" + // set background size
+                    "-fx-background-position: left center;" + // set background position
+                    "");
 
-		newLook.setOnAction(event -> {
-			newLookNum = (newLookNum + 1) % 4;
-			String newBackgroundPath = "/KenoBackground" + newLookNum + ".jpg";
-			root.setStyle("-fx-background-image: url('" + newBackgroundPath + "');" +
-					"-fx-background-size: cover;" +
-					"-fx-background-position: left center;");
-			// Randomize font
-			String[] fonts = {"Arial", "Comic Sans MS", "Verdana", "Trebuchet MS"};
-			String randomFont = fonts[newLookNum];
-			welcomeText.setFont(Font.font(randomFont, FontWeight.BOLD, 30));
+            // Randomize font
+            String[] fonts = {"Arial", "Comic Sans MS", "Verdana", "Trebuchet MS"}; // create array of fonts
+            String randomFont = fonts[newLookNum]; // get random font
+            welcomeText.setFont(Font.font(randomFont, FontWeight.BOLD, 30)); // set font of welcome text
 
-			// Randomize color
-			Color[] colors = {Color.ORANGE, Color.AQUA, Color.YELLOW, Color.GREEN};
-			Color randomColor = colors[newLookNum];
-			welcomeText.setFill(randomColor);
-			welcomeText.setStroke(randomColor.darker());
+            // Randomize color
+            Color[] colors = {Color.ORANGE, Color.AQUA, Color.YELLOW, Color.GREEN}; // create array of colors
+            Color randomColor = colors[newLookNum]; // get random color
+            welcomeText.setFill(randomColor); // set fill color of welcome text
+            welcomeText.setStroke(randomColor.darker()); // set stroke color of welcome text
 
-		});
+        });
 
-		exit.setOnAction(event -> primaryStage.close());
-	}
-
+        exit.setOnAction(event -> primaryStage.close()); // add event handler to exit menu item
+    }
 }
