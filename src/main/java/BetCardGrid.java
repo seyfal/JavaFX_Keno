6235c5a1ea7b1abeb143a74bae248b5b2b61881f
Seyfal Sultanov <ssulta24@uic.edu>
2023-03-15 03:01:38 -0500

    Made a lot of changes

--- src/main/java/BetCardGrid.java
+++ src/main/java/BetCardGrid.java
@@ -1,17 +1,5 @@
-import javafx.animation.ScaleTransition;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.RadialGradient;
-import javafx.scene.paint.Stop;
-import javafx.scene.shape.Circle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.util.Duration;
 
 /*
  BetCardGrid is a JavaFX GridPane that displays a bet card with 80 numbers. The player can select numbers on the bet
@@ -44,17 +32,7 @@ import javafx.util.Duration;
  * Represents a grid of clickable circles for selecting numbers on the bet card.
  */
 class BetCardGrid extends GridPane {
-
-    // variables:
-    private int numSpots; // the number of spots the player has chosen to play.
-    private int[] spots; // an array to store the numbers that the player has selected on the bet card
-    private int numSelected; // the number of spots that the player has currently selected
     private int maxSelected; // the maximum number of spots that the player is allowed to select based on their initial choice of numSpots
-    private GridPane gridPane; // the JavaFX GridPane that will be used to display the bet card
-//    private static final int ROWS = 10; // the number of rows in the bet card grid
-//    private static final int COLS = 8; // the number of columns in the bet card grid
-
-    // functions:
 
     /**
      * This constructor initializes the instance variables for the BetCardGrid object,
@@ -63,7 +41,6 @@ class BetCardGrid extends GridPane {
      * deselect numbers. Finally, all cells in the GridPane are initially disabled.
      *
      */
-
     public BetCardGrid(int rows, int columns) {
         setHgap(12); // Increase horizontal spacing between balls
         setVgap(12); // Increase vertical spacing between balls
@@ -72,109 +49,19 @@ class BetCardGrid extends GridPane {
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                BetButton betButton = new BetButton(number);
+                BetButton betButton = createBetButton(number);
                 add(betButton, j, i);
                 number++;
             }
         }
-        // disableAll();
     }
 
     /**
-     * This function checks if the given number is already selected on the bet card.
-     * It should return a boolean value true if the number is selected and false otherwise.
-     *
-     * @param number the number to check
-     * @return true if the number is selected, false otherwise
+     * This function creates a BetButton object for the given number.
+     * @return BetButton
      */
-    public boolean isSelected(int number) {
-        for (int i = 0; i < numSelected; i++) { // looping through the numbers that the player has selected on the bet card
-            if (spots[i] == number) { // if the number that the player has selected on the bet card is already selected
-                return true; // return true
-            }
-        }
-        return false; // return false if the number that the player has selected on the bet card is not already selected
-    }
-
-    /**
-     * This function adds the given number to the list of selected spots on the bet card.
-     * If the maximum number of spots has already been selected, the function should do nothing.
-     *
-     * @param number the number to select
-     */
-    public void select(int number) {
-        if (numSelected < maxSelected) { // if the maximum number of spots has not already been selected
-            spots[numSelected] = number; // add the number to the list of selected spots on the bet card
-            numSelected++; // increment the numSelected instance variable
-            updateCell(number, true); // calling the updateCell() method
-        }
-    }
-
-    /**
-     * This function removes the given number from the list of selected spots on the bet card.
-     *
-     * @param number the number to deselect
-     */
-    public void deselect(int number) {
-        for (int i = 0; i < numSelected; i++) { // looping through the numbers that the player has selected on the bet card
-            if (spots[i] == number) { // if the number that the player has selected on the bet card is already selected
-                for (int j = i; j < numSelected - 1; j++) { // looping through the numbers that the player has selected on the bet card
-                    spots[j] = spots[j + 1]; // removing the number from the list of selected spots on the bet card
-                }
-                spots[numSelected - 1] = 0; // setting the last element in the spots array to 0
-                numSelected--; // decrement the numSelected instance variable
-                updateCell(number, false); // calling the updateCell() method
-                break; // break out of the loop
-            }
-        }
-    }
-
-    /**
-     * This function disables all the cells in the bet card.
-     */
-    public void disableAll() {
-        for (Node node : this.getChildren()) { // looping through the cells in the bet card
-            node.setDisable(true); // disabling the cells in the bet card
-        }
-    }
-
-    /**
-     * This function enables all the cells in the bet card.
-     */
-    public void enableAll() {
-        for (Node node : this.getChildren()) { // looping through the cells in the bet card
-            node.setDisable(false); // enabling the cells in the bet card
-        }
-    }
-
-    /**
-     * This function returns an array of integers representing the selected spots on the bet card.
-     *
-     * @return an array of integers representing the selected spots on the bet card
-     */
-    public int[] getSelectedSpots() {
-        int[] selectedSpots = new int[numSelected]; // creating an array of integers to store the numbers that the player has selected on the bet card
-        System.arraycopy(spots, 0, selectedSpots, 0, numSelected); // copying the numbers that the player has selected on the bet card to the selectedSpots array
-        return selectedSpots; // returning the selectedSpots array
-    }
-
-    /**
-     * This function updates the visual appearance of the cell corresponding
-     * to the given number on the bet card based on whether it is selected or not.
-     */
-    private void updateCell(int number, boolean selected) {
-        for (Node node : this.getChildren()) {
-            if (node instanceof Button) {
-                Button btn = (Button) node;
-                if (btn.getText().equals(String.valueOf(number))) {
-                    if (selected) {
-                        btn.setStyle("-fx-background-radius: 20; -fx-padding: 5; -fx-focus-color: transparent; -fx-faint-focus-color: transparent; -fx-background-color: #ffffff; -fx-border-color: #0000ff; -fx-border-width: 2; -fx-border-radius: 20; -fx-font-family: System; -fx-font-size: 12");
-                    } else {
-                        btn.setStyle("-fx-background-radius: 20; -fx-padding: 5; -fx-focus-color: transparent; -fx-faint-focus-color: transparent; -fx-background-color: #ffffff; -fx-border-color: transparent; -fx-font-family: System; -fx-font-size: 12");
-                    }
-                }
-            }
-        }
+    private BetButton createBetButton(int number) {
+        return new BetButton(number);
     }
 
     /**
@@ -183,6 +70,5 @@ class BetCardGrid extends GridPane {
     public void setMaxSpots(int maxSpots) {
         this.maxSelected = maxSpots;
     }
-
 
 }
--- src/main/java/KenoController.java
+++ src/main/java/KenoController.java
@@ -3,93 +3,85 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-import java.util.ResourceBundle;
-
 public class KenoController {
+
     public void initializeKenoUI(BorderPane root) {
-        // Create winnings column (left side)
+        VBox winningsColumn = createWinningsColumn();
+        root.setLeft(winningsColumn);
+
+        BetCardGrid betCardGrid = new BetCardGrid(8, 10);
+        winningsColumn.prefHeightProperty().bind(betCardGrid.heightProperty());
+
+        HBox topLayout = new HBox(20);
+        topLayout.setAlignment(Pos.CENTER);
+        topLayout.getChildren().addAll(winningsColumn, betCardGrid);
+
+        HBox buttonLayout = createButtonLayout();
+
+        VBox mainLayout = new VBox(30);
+        mainLayout.getChildren().addAll(topLayout, buttonLayout);
+        mainLayout.setAlignment(Pos.CENTER);
+        mainLayout.setPadding(new Insets(20, 20, 20, 20));
+
+        root.setCenter(mainLayout);
+    }
+
+    private VBox createWinningsColumn() {
         VBox winningsColumn = new VBox(10);
         winningsColumn.setSpacing(10);
         winningsColumn.setPadding(new Insets(10, 10, 10, 10));
         winningsColumn.setStyle("-fx-background-color: white;");
+
         for (int i = 1; i <= 10; i++) {
             HBox row = new HBox(5);
             row.getChildren().addAll(new Label("Spots: " + i), new Label("Winnings: $..."));
             winningsColumn.getChildren().add(row);
         }
 
-        // Add an empty box to fill up the remaining space below winningsColumn
         VBox emptyBox = new VBox();
         VBox.setVgrow(emptyBox, Priority.ALWAYS);
         winningsColumn.getChildren().add(emptyBox);
 
-        root.setLeft(winningsColumn);
+        return winningsColumn;
+    }
 
-        // Create bet card grid (center)
-        BetCardGrid betCardGrid = new BetCardGrid(8, 10);
-
-        // Bind the height of the winningsColumn to the height of the betCardGrid
-        winningsColumn.prefHeightProperty().bind(betCardGrid.heightProperty());
-
-        // Spots and Draws blocks
+    private HBox createButtonLayout() {
         VBox spotsBlock = createButtonBlock("Spots", new int[][]{{1, 4}, {8, 10}});
         VBox drawsBlock = createButtonBlock("Draws", new int[][]{{1, 2}, {3, 4}});
+        VBox autoPlayBox = createAutoPlayBox();
 
-        // Auto and Play buttons
-        Button autoButton = new CustomButton("Auto");
-        Button playButton = new CustomButton("Play");
-
-        // Add Auto and Play buttons to a vertical box
-        VBox autoPlayBox = new VBox(10);
-        autoPlayBox.setAlignment(Pos.CENTER);
-        autoPlayBox.getChildren().addAll(autoButton, playButton);
-
-        // Add an empty box to fill up the remaining space above autoPlayBox
-        VBox emptyBoxAboveButtons = new VBox();
-        VBox.setVgrow(emptyBoxAboveButtons, Priority.ALWAYS);
-        autoPlayBox.getChildren().add(0, emptyBoxAboveButtons);
-
-        // Wrap winningsColumn and betCardGrid in a VBox
-        VBox leftSide = new VBox();
-        leftSide.getChildren().addAll(winningsColumn, new Region()); // Add an empty Region to fill the remaining space
-        VBox.setVgrow(winningsColumn, Priority.ALWAYS);
-        VBox.setVgrow(new Region(), Priority.ALWAYS);
-
-        // Combine winningsColumn and betCardGrid in an HBox
-        HBox topLayout = new HBox(20);
-        topLayout.setAlignment(Pos.CENTER);
-        topLayout.getChildren().addAll(winningsColumn, betCardGrid);
-
-        // Combine Spots, Draws, and Auto/Play buttons in a horizontal layout
         HBox buttonLayout = new HBox(20);
         buttonLayout.setAlignment(Pos.CENTER);
         buttonLayout.getChildren().addAll(spotsBlock, drawsBlock, autoPlayBox);
 
-        // Set horizontal grow priority to make buttons take up the whole width
         HBox.setHgrow(spotsBlock, Priority.ALWAYS);
         HBox.setHgrow(drawsBlock, Priority.ALWAYS);
         HBox.setHgrow(autoPlayBox, Priority.ALWAYS);
 
-        // Add the top layout and button layout to the main layout with increased spacing
-        VBox mainLayout = new VBox(30); // Increase spacing here
-        mainLayout.getChildren().addAll(topLayout, buttonLayout);
-        mainLayout.setAlignment(Pos.CENTER);
-        mainLayout.setPadding(new Insets(20, 20, 20, 20)); // Add padding here
-
-        // Set the main layout as the center of the root
-        root.setCenter(mainLayout);
-
-
+        return buttonLayout;
     }
 
-    // KenoController.java
+    private VBox createAutoPlayBox() {
+        Button autoButton = new CustomButton("Auto");
+        Button playButton = new CustomButton("Play");
+
+        VBox autoPlayBox = new VBox(10);
+        autoPlayBox.setAlignment(Pos.CENTER);
+        autoPlayBox.getChildren().addAll(autoButton, playButton);
+
+        VBox emptyBoxAboveButtons = new VBox();
+        VBox.setVgrow(emptyBoxAboveButtons, Priority.ALWAYS);
+        autoPlayBox.getChildren().add(0, emptyBoxAboveButtons);
+
+        return autoPlayBox;
+    }
+
     private VBox createButtonBlock(String title, int[][] numbers) {
         Label blockTitle = new Label(title);
-        blockTitle.setStyle("-fx-font-size: 24; -fx-font-weight: bold; -fx-text-fill: #333;"); // Update the text style
+        blockTitle.setStyle("-fx-font-size: 24; -fx-font-weight: bold; -fx-text-fill: #333;");
         blockTitle.setMaxWidth(Double.MAX_VALUE);
         blockTitle.setAlignment(Pos.CENTER);
 
@@ -110,5 +102,6 @@ public class KenoController {
 
         return buttonBlock;
     }
-
 }
+
+
